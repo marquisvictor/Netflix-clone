077fbfefb87c8c7755c562907aad63197f18cfde
Victor Irekponor <marquisvictor93@gmail.com>
2021-12-10 21:38:03 +0100

    add the functionality for signin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Login } from './components';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Landing } from './pages';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './app.css';
 
 function App() {
     return (
         <div className=''>
-            <Login />
+            <Landing />
         </div>
     );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,1 @@
-export { default as Login } from './login/Login';
+export { default as SignIn } from './signin/SignIn';
